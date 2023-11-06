7d77fa61d650e82e2fd29a471f17a844a9a6625a
Ali Gowi <75486108+alialdwahy@users.noreply.github.com>
2023-11-06 11:47:55 +0300

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongo = require('mongoose');
-const baseURL = 'mongodb+srv://gowi:paris055866@cluster0.x4jpf.mongodb.net/ChatTest?retryWrites=true&w=majority';
+const baseURL = 'URL';
 mongo.connect(process.env.MONGODB_URI || baseURL,{useUnifiedTopology: true,  useCreateIndex: true, useNewUrlParser: true}).then((db)=>{
     console.log('Connected MONGODB');
 }).catch((error)=> console.log('Not Connected'));
